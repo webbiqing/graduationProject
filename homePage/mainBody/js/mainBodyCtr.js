dafb00ddfe0b7f8777b10423ecb6d315cfbe90a7
webbiqing <webwbqing@163.com>
2017-02-14 10:38:48 +0800

    提交找回的修改

--- homePage/mainBody/js/mainBodyCtr.js
+++ homePage/mainBody/js/mainBodyCtr.js
@@ -2,5 +2,14 @@
  * 主体部分控制器
  */
 App.controller('mainBodyCtr', function($rootScope, $scope) {
-
+    $scope.swiper = {};
+    $scope.next = function() {
+        $scope.swiper.slideNext();
+    };
+    $scope.onReadySwiper = function(swiper) {
+        console.log('onReadySwiper');
+        swiper.on('slideChangeStart', function() {
+            console.log('slideChangeStart');
+        });
+    };
 });
